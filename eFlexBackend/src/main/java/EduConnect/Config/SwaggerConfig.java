61f311071e973d70ef171e733ed9b7eb7b97649c
Nguyen Toan Thang <xamthang09@gmail.com>
2025-05-25 11:57:49 +0700

    fix swagger authorization

--- eFlexBackend/src/main/java/EduConnect/Config/SwaggerConfig.java
+++ eFlexBackend/src/main/java/EduConnect/Config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package EduConnect.Config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,14 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("EduConnect API")
                         .version("1.0.0")
-                        .description("API documentation for EduConnect application"));
+                        .description("API documentation for EduConnect application"))
+                // Thêm Security Scheme cho Bearer Token
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                // Áp dụng yêu cầu bảo mật cho tất cả các API
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
