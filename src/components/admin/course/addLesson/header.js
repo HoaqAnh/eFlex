530a7852996c7be5d1a5883af0aa32c2a981d98b
Hoang Anh <288hoanganh@gmail.com>
2025-05-28 01:16:31 +0700

    Remove unnecessary files

--- src/components/admin/course/addLesson/header.js
+++
@@ -1,9 +0,0 @@
-import "../../../../styles/admin/addLesson/header.css";
-
-const Header = () => (
-    <div className="addLesson-header">
-        <h1>Thêm bài học</h1>
-    </div>
-);
-
-export default Header;
